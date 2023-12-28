6dec0629b9cac8f2b12c9c18606ba73b48304d00
Tommy Chen <tommyjchen@hotmail.com>
2023-12-28 18:28:56 -0500

    updated example

--- src/example.js
+++ src/example.js
@@ -1,30 +1,46 @@
 import { v4 as uuid } from "uuid";
 const schoolID = uuid();
+const schoolID2 = uuid();
 const workID = uuid();
+const workID2 = uuid();
 const example = {
   info: {
-    name: "Sarah Goode",
-    email: "sarahgoode@example.com ",
+    name: "John Smith",
+    email: "johnsmith@email.com ",
     phone: "555-555-0000",
   },
   edu: {
     [schoolID]: {
-      place: "State College of Florida",
-      title: "General Education",
+      place: "Riverside University",
+      title: "Bachelor's in computer science",
       start: "July 2018",
       end: "Jan 2020",
-      location: "Florida, US",
+    },
+    [schoolID2]: {
+      place: "Lakeview Community College",
+      title: "General Education",
+      start: "2016",
+      end: "2018",
     },
   },
   prof: {
     [workID]: {
-      place: "Safran Power",
-      title: "Logistic Coordinator",
+      place: "BlueNet Technology Group",
+      title: "Java Developer",
       details:
-        "Responsible for all warehouse related task; from managing inventory, to all dispatch related activities, such as picking, packing, shipping for both domestic and international customers.",
-      start: "Jun 2015",
+        "Participate in lifecycle development for web application. Troubleshoot website codes to identify and correct errors. Collaborate with customers to identify project needs and technical concerns.",
+      start: "Jun 2021",
       end: "Present",
-      location: "Florida, US",
+      location: "Somewhere, US",
+    },
+    [workID2]: {
+      place: "Windell Design Work",
+      title: "UX Designer",
+      details:
+        "Designed product packaging, created online campaigns, and worked on UI development for web/mobile.",
+      start: "2019",
+      end: "Jun 2021",
+      location: "Somwhere, US",
     },
   },
 };
